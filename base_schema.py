4ec9358de9082fc887966fe891b6747cfe803b71
Vaibhav Pandey <vaibhap1@uci.edu>
2022-03-07 23:21:07 -0800

    added events api v1

--- base_schema.py
+++ base_schema.py
@@ -1,5 +1,6 @@
 from sqlalchemy import BigInteger, Column, Float, TIMESTAMP
 from sqlalchemy.types import Integer, Numeric, String
+from sqlalchemy.dialects.postgresql import JSON
 base_schema = {
     "integer_datastream_schema.avsc": {
         "individual_id": Column(String, primary_key=True),
@@ -24,5 +25,13 @@ base_schema = {
         "value": Column(String),
         "unit": Column(String),
         "confidence": Column(String, default=None)
+        },
+    "event_schema.avsc": {
+        "user_id": Column(String, primary_key=True),
+        "start_time": Column(TIMESTAMP, primary_key=True),
+        "end_time": Column(TIMESTAMP, primary_key=True),
+        "event_name": Column(String, primary_key=True),
+        "source": Column(String, primary_key=True),
+        "parameters": Column(JSON, primary_key=True)
         }
 }
