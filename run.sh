21ec873743512f5fade92d16e2dd129d2cf40aee
vaibhav-clearsense <97196207+vaibhav-clearsense@users.noreply.github.com>
2022-03-09 10:22:29 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ source env/bin/activate
 
 pip install -r requirements.txt
 
-uvicorn --port 3000 --host 0.0.0.0 main:app 
+uvicorn --port 8000 --host 0.0.0.0 main:app 
 # uvicorn --ssl-certfile certificates/cert.pem --ssl-keyfile certificates/key.pem --port 3000 --host 0.0.0.0 main:app 
